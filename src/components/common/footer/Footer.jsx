ee189db67813cbba932fa41746124f14243bfd8a
Vijay Prasath B <vijaycreatives@users.noreply.github.com>
2026-06-20 13:40:08 +0530

    Fix footer links to use react-scroll Link (prevents hash router conflict)

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,4 +1,5 @@
 import logo from "../../../assets/logo.png";
+import { Link } from "react-scroll";
 
 const navItems = [
   { id: 1, name: "Home", url: "introduction" },
@@ -16,22 +17,29 @@ const Footer = () => {
   return (
     <div className="pt-25 md:pt-40 content max-2xl:px-3">
       <div className="flex max-md:flex-col justify-between mx-0 items-center h-full w-full text-neutral-200">
-        <a href="#introduction" className="flex items-center border-0">
+        <Link
+          to="introduction"
+          smooth={true}
+          duration={900}
+          className="flex items-center border-0"
+        >
           <img src={logo} className="h-8 sm:h-14 rounded-2xl" alt="logo" />
           <p className="text-3xl sm:text-[32px] my-auto ms-[12px] font-semibold">
             Vijay
           </p>
-        </a>
+        </Link>
         <div className="mx-7 max-md:my-7 text-center">
           {navItems.map((item) => (
-            <a
+            <Link
               key={item.id}
-              className="mx-2 group inline-block relative w-fit text-[12px] sm:text-[16px]"
-              href={`#${item.url}`}
+              to={item.url}
+              smooth={true}
+              duration={900}
+              className="mx-2 group inline-block relative w-fit text-[12px] sm:text-[16px] cursor-pointer"
             >
               {item.name}
               <span className="absolute left-0 bottom-0 h-0.5 w-full bg-white scale-x-0 duration-300 group-hover:scale-x-100"></span>
-            </a>
+            </Link>
           ))}
         </div>
         <p className="text-[12px] sm:text-[16px]">
