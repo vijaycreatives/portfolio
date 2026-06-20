5676a9570f74182607032edc68dadcf2df4d4e8e
Vijay Prasath B <vijaycreatives@users.noreply.github.com>
2026-06-20 13:27:17 +0530

    Fix child route to use index:true, add errorElement

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,27 +1,24 @@
 import { lazy, Suspense } from "react";
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import Loading from "../components/common/loading/Loading";
+
 const Home = lazy(() => import("../pages/Home"));
 const Main = lazy(() => import("../layouts/Main"));
 
-const repoName = import.meta.env.VITE_REPO_NAME || "";
-
-export const router = createBrowserRouter(
-  [
-    {
-      path: `/`,
-      element: (
-        <Suspense fallback={<Loading />}>
-          <Main />
-        </Suspense>
-      ),
-      children: [
-        {
-          path: "/",
-          element: <Home></Home>,
-        },
-      ],
-    },
-  ],
-  { basename: `/${repoName}` }
-);
+export const router = createHashRouter([
+  {
+    path: "/",
+    element: (
+      <Suspense fallback={<Loading />}>
+        <Main />
+      </Suspense>
+    ),
+    errorElement: <div className="flex items-center justify-center min-h-screen text-xl">Something went wrong</div>,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+]);
